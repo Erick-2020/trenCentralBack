c419ee6dca06d06e67d7cbfe3b3dad994937a12e
new-page-rh <leonardoraerick20@gmail.com>
2024-05-18 09:49:35 -0500

    :rotating_light: Eliminamos archivos no necesarios :rotating_light:

--- src/main/java/com/apiTren/crudJava/repositories/IdTrenesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apiTren.crudJava.repositories;
-
-import com.apiTren.crudJava.models.TrenesModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IdTrenesRepository extends JpaRepository<TrenesModel, Long> {
-}
